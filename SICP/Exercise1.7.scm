bdcc535d3d9609dcae1a9104dee96e27a1945c5f
Alex Shapiro <sirshaps@gmail.com>
2016-11-06 14:59:02 -0800

    Commit #2!

---
+++ SICP/Exercise1.7.scm
@@ -0,0 +1,25 @@
+(define (square x)
+  (* x x))
+(define (abs x)
+  (if (< x 0)
+      (- 0 x)
+      x))
+(define (sqrt-test x)
+  (sqrt-iter 1.0 x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (good-enough?2.0 guess-old guess-new)
+  (< (abs (- guess-new guess-old)) (* 0.001 guess-new)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+(define (improve guess x)
+  (average guess (/ x guess)))
+(define (sqrt-iter guess x)
+  (if (good-enough?2.0 guess (improve guess x))
+      guess
+      (sqrt-iter (improve guess x)
+		 x)))
+(sqrt-test 6)
